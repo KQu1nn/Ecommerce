b6bf621030f72019871a84d44424d14d34f01980
Ramon Kelvin <themastertechbr@gmail.com>
2025-07-30 08:50:52 -0300

    First commit - Ecommerce Vue + Tailwind

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <main>
+    <ProductForm
+      class="w-full h-full flex flex-col"
+      @submitProduct="handleProduct"
+      />
+  </main>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import ProductForm from './components/ProductForm.vue';
+import ProductCards from './components/ProductCards.vue';
+import ShoppingCart from './components/ShoppingCart.vue';
+
+const products = ref([])
+
+function handleProduct(product) {
+  products.value.push(product);
+}
+
+console.log(products.value)
+</script>
---
+++ src/components/ProductForm.vue
@@ -0,0 +1,42 @@
+<template>
+    <main>
+        <h2>Ecommerce</h2>
+        <form @submit.prevent="send">
+            <input v-model="productName" type="text" placeholder="Nome do produto..">
+            <input v-model="productPrice" type="number" placeholder="Preço..">
+            <button>Cadastrar Produto</button>
+            <p v-if="userFeedback">{{ userFeedback }}</p>
+        </form>
+    </main>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const productName = ref('');
+const productPrice = ref(null);
+const userFeedback = ref('');
+const emit = defineEmits(['submitProduct']);
+
+function send() {
+    if(!productName.value || !productPrice.value) {
+        userFeedback.value = 'Por favor, preencha todos os dados!';
+    } else {
+        emit('submitProduct', {
+            name: productName.value,
+            price: productPrice.value
+        });
+    }
+    resetFields();
+    resetUserFeedback();
+}
+function resetFields() {
+    productName.value = '';
+    productPrice.value = '';
+}
+function resetUserFeedback() {
+    setTimeout(() => {
+        userFeedback.value = '';
+    }, 3000)
+}
+</script>
